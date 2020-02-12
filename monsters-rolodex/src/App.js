f4d249722b40bcf580676570b528ff7125fecf6a
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-02-12 01:35:13 +0100

    add card, car-list components - add jsx definitions - add css definitions - add search bar, read the input and update state

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      string: 'Hello Alex'
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-          <button onClick={() => this.setState({ string: 'Hi Alex'})}>
-            Change Text
-          </button>
-        </header>
+        <input type='search' placeholder='search monsters' onChange={e => {this.setState( { searchField: e.target.value } )}}/>
+        <CardList monsters={this.state.monsters}>
+        </CardList>
       </div>
     );
   }
